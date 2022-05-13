f8cde01699d62e9353c78a1ea82e05aaf56380ce
Sam Senior <samuelsenior@developersinstitute.co.nz>
2022-05-13 17:33:33 +1200

    tidying

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import Feature from '../../components/feature/Feature';
-import './services.css';
+import React from "react";
+import Feature from "../../components/feature/Feature";
+import "./services.css";
 
 const featuresData = [
   {
-    title: 'Audit Services',
-    text: 'We provide a highly extensive audit service that will help teams reach their full potential to capitalise on all aspects of a release',
+    title: "Audit Services",
+    text: "We provide a highly extensive audit service that will help teams reach their full potential to capitalise on all aspects of a release",
   },
   {
-    title: 'Tokenomics calculators',
-    text: 'We offer this free service to our customers to help the mlearn and understand tokenomics, which plays a cruical part of the blockhain gaming sector.',
+    title: "Tokenomics Calculators",
+    text: "We offer this free service to our customers to help the mlearn and understand tokenomics, which plays a cruical part of the blockhain gaming sector.",
   },
   {
-    title: 'Report writing',
-    text: 'We have a team of wolves constantly assessing and sharing about the latest advancements and projects',
+    title: "Report Writing",
+    text: "We have a team of wolves constantly assessing and sharing about the latest advancements and projects",
   },
   {
-    title: 'Marketing',
-    text: 'We aim for excellence and will continue to better our brand, team and vison',
+    title: "Marketing",
+    text: "We aim for excellence and will continue to better our brand, team and vison",
   },
   {
-    title: 'Curated Education',
-    text: 'Education is the backbone of our ethos, we will always strive to expand the significance of this subject and teach the public how to educate themselves',
+    title: "Curated Education",
+    text: "Education is the backbone of our ethos, we will always strive to expand the significance of this subject and teach the public how to educate themselves",
   },
 ];
 
 const roadmap = () => (
   <div className="features section__padding" id="services">
     <div className="features-heading">
-      <h1 className="gradient__text">We will provide services and continue to expand as we strive to be the worlds leading resource for builders, investors and communities creating the web3.0 games of tomorrow.</h1>
+      <h1 className="gradient__text">
+        We will provide services and continue to expand as we strive to be the
+        worlds leading resource for builders, investors and communities creating
+        the web3.0 games of tomorrow.
+      </h1>
       <p></p>
     </div>
     <div className="features-container">
