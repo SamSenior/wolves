8ebc461dbd52456e3349ade818f9d737c9c33758
Sam Senior <samuelsenior@developersinstitute.co.nz>
2022-05-13 17:30:59 +1200

    changed font size

--- src/containers/roadmap/Roadmap.jsx
+++ src/containers/roadmap/Roadmap.jsx
@@ -8,13 +8,17 @@ const Roadmap = () => (
       <img src={vrImage} alt="vr" />
     </div>
     <div className="roadmap-content">
-      <h1 className="gradient__text">
-        Roadmap
-      </h1>
-      <h4>We believe in what Aristotle said that 'the whole is greater than the sum of its parts.' This quote speaks to the importance of synergy and wholeness in order for health and longevity. The Wolves DAO takes a top-down and bottom-up approach in aims to move the gaming space forward in a holistic manner. 
-        </h4>
+      <h1 className="gradient__text">Roadmap</h1>
+      <h4>
+        We believe in what Aristotle said that 'the whole is greater than the
+        sum of its parts.' This quote speaks to the importance of synergy and
+        wholeness in order for health and longevity. The Wolves DAO takes a
+        top-down and bottom-up approach in aims to move the gaming space forward
+        in a holistic manner.
+      </h4>
       <p>
-        Want to know what we have to offer and where we see ourselves in the future? Read our roadmap to find out more
+        Want to know what we have to offer and where we see ourselves in the
+        future?
       </p>
       <button type="button">View Roadmap</button>
     </div>
